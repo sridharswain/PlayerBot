2cfd12939418dd84975d57763bbe8f6a6ce8dca9
sridharswain <sridharswain25@hotmail.com>
2018-07-22 17:27:58 +0530

    comments added to game.py

--- Game/ChromeDino/game.py
+++ Game/ChromeDino/game.py
@@ -3,23 +3,24 @@ import pygame
 from player import Player
 from obstacle import Obstacle
 
-display_width = 1300
-display_height = 500
+display_width = 1300  #DISPLAY
+display_height = 500  #DIMENSIONS
 player_init_position_X = 50
 player_init_position_Y = 150
 obstacle_init_position_X = 1200
+obstacle_init_position_Y = 300
 white = (255,255,255)
 currentY=player_init_position_Y
-playerDirection = 0
-playerJumpHeight = player_init_position_Y-90
-playerJumpStrength = 4
-obstacle_spawn_rate = 160
-obstacle_movement_rate = 3.5
-score = 0
-reward = 1
-rewardRate = 20
-nCycles = 0
-obstacles = []
+playerDirection = 0 # 0 : PLAYER AT GROUND; -1 : PLAYER IS GOING UP; 1 : PLAYER IS GOING DOWN
+playerJumpHeight = player_init_position_Y-90 # HEIGHT TO WHICH AGENT IS GOING TO JUMP
+playerJumpStrength = 4 # RATE WHICH THE TREX MOVES UP AND DOWN
+obstacle_spawn_rate = 160 # OBSTACLE APPEARS AFTER EVERY 160 FRAMES
+obstacle_movement_rate = 3.5 # RATE AT WHICH OBSTACLES MOVE TOWARDS THE PLAYER
+score = 0 # CURRENT SCORE
+reward = 1 # AMOUNT TO BE ADDED TO SCORE
+rewardRate = 20 # AFTER 20 FRAMES REWARD IS GOING TO BE ADDED TO SCORE
+nCycles = 0 #NUMBER OF CYCLES COMPLETED
+obstacles = [] #LIST OF ALL OBSTACLE OF THE SCENE
 
 #INITIATION
 pygame.init()
@@ -33,7 +34,6 @@ gameDisplay.fill(white)
 
 agent = Player("res/agent.png",gameDisplay,clock)
 agent.setPlayerAt(player_init_position_X,player_init_position_Y)
-#agent.setPlayerAt(player_init_position_X+100,player_init_position_Y)
 
 def updateFrame():
     pygame.display.update()
@@ -49,13 +49,11 @@ def giveReward():
 
 def addObstacle():
     cactus = Obstacle(gameDisplay,clock)
-    cactus.addToDisplay(obstacle_init_position_X,300)
+    cactus.addToDisplay(obstacle_init_position_X,obstacle_init_position_Y)
     global obstacles
     obstacles.append(cactus)
     global nCycles
     nCycles=0
-
-#def resetCycles
 
 def moveObstaclesLeft():
     global obstacles
@@ -77,13 +75,13 @@ while not crashed:
         if(event.type == pygame.QUIT):
             crashed=True
         elif (event.type==pygame.KEYDOWN and event.key == pygame.K_SPACE):
-            jump()
+            jump() #START JUMP ACTION
 
     currentY +=playerDirection
     agent.setPlayerAt(player_init_position_X,player_init_position_Y+currentY)
 
     if(nCycles%rewardRate == 0):
-        giveReward()
+        giveReward() # INCREASE THE SCORE OF THE PLAYER
 
     if(nCycles%obstacle_spawn_rate == 0):
         print("added")
