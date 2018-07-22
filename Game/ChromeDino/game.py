fbfa4f493bd8b1d7750190be377744f35bdc97b4
sridharswain <sridharswain25@hotmail.com>
2018-07-22 16:56:35 +0530

    Obstacles added

--- Game/ChromeDino/game.py
+++ Game/ChromeDino/game.py
@@ -1,12 +1,26 @@
 #!/usr/bin/env python3
 import pygame
 from player import Player
+from obstacle import Obstacle
 
 display_width = 1300
-display_height = 300
+display_height = 500
 player_init_position_X = 50
 player_init_position_Y = 150
+obstacle_init_position_X = 1200
 white = (255,255,255)
+currentY=player_init_position_Y
+playerDirection = 0
+playerJumpHeight = player_init_position_Y-90
+playerJumpStrength = 4
+obstacle_spawn_rate = 160
+obstacle_movement_rate = 3.5
+score = 0
+reward = 1
+rewardRate = 20
+nCycles = 0
+obstacles = []
+
 #INITIATION
 pygame.init()
 
@@ -21,6 +35,39 @@ agent = Player("res/agent.png",gameDisplay,clock)
 agent.setPlayerAt(player_init_position_X,player_init_position_Y)
 #agent.setPlayerAt(player_init_position_X+100,player_init_position_Y)
 
+def updateFrame():
+    pygame.display.update()
+    global nCycles
+    nCycles+=1
+    clock.tick(100)
+
+def giveReward():
+    global nCycles
+    #nCycles=0
+    global score
+    score+=reward
+
+def addObstacle():
+    cactus = Obstacle(gameDisplay,clock)
+    cactus.addToDisplay(obstacle_init_position_X,300)
+    global obstacles
+    obstacles.append(cactus)
+    global nCycles
+    nCycles=0
+
+#def resetCycles
+
+def moveObstaclesLeft():
+    global obstacles
+    for cactus in obstacles:
+        cactusX = cactus.moveLeftBy(obstacle_movement_rate)
+        if(cactusX <= 0):
+            obstacles.remove(cactus)
+
+def jump():
+    global playerDirection
+    playerDirection = -1*playerJumpStrength
+
 
 crashed = False
 x = player_init_position_X
@@ -29,11 +76,30 @@ while not crashed:
     for event in pygame.event.get():
         if(event.type == pygame.QUIT):
             crashed=True
-        elif (event.type==pygame.KEYDOWN and event.key==pygame.K_SPACE):
-            if(agent.getPlayerPosition()[1]==player_init_position_Y):
-                agent.jump(26)
-    agent.setPlayerAt(x,player_init_position_Y)
-    pygame.display.update()
-    clock.tick(400)
+        elif (event.type==pygame.KEYDOWN and event.key == pygame.K_SPACE):
+            jump()
+
+    currentY +=playerDirection
+    agent.setPlayerAt(player_init_position_X,player_init_position_Y+currentY)
+
+    if(nCycles%rewardRate == 0):
+        giveReward()
+
+    if(nCycles%obstacle_spawn_rate == 0):
+        print("added")
+        addObstacle() #ADD A NEW OBSTACLE TO THE SCENCE
+
+    moveObstaclesLeft() # MOVE OBSTACLES TOWARDS LEFT
+
+    if(currentY>=player_init_position_Y):
+        playerDirection=0
+        currentY = player_init_position_Y
+    elif currentY<=playerJumpHeight:
+        playerDirection=playerJumpStrength
+
+    updateFrame() #DRAW ALL THE ELEMENTS OF THE SCENE ON THE SCREEN
+
+
+
 pygame.quit()
 quit()
--- Game/ChromeDino/obstacle.py
+++ Game/ChromeDino/obstacle.py
@@ -2,14 +2,33 @@ import pygame
 import random
 
 class Obstacle:
-    images = ["obstacle_large_3","obstacle_large_2","obstacle_small_2","obstacle_small_3","obstacle_small_1"]
     def __init__(self,gameDisplay,clock):
-        imgPath =random.choice(images,1)
+        images = ["obstacle_small_1","obstacle_small_2","obstacle_small_3","obstacle_large_2","obstacle_large_3"]
+        randomInt = random.randint(0,11)
+        if(randomInt < 3):
+            imgPath = 0
+        elif(randomInt < 6):
+            imgPath = 1
+        elif(randomInt < 8):
+            imgPath = 2
+        elif(randomInt < 10):
+            imgPath = 3
+        else:
+            imgPath = 4
+        imgPath = "res/"+images[imgPath]+".png"
         self.obstacle = pygame.image.load(imgPath)
         self.gameDisplay = gameDisplay
         self.clock=clock
+        self.imageSize = self.obstacle.get_rect().size
+        print(self.imageSize)
 
     def addToDisplay(self,x,y):
-        gameDisplay.fill((255,255,255))
-        self.gameDisplay.blit(self.obstacle,(x,y))
-        pygame.display.update()
+        #gameDisplay.fill((255,255,255))
+        self.gameDisplay.blit(self.obstacle,(x,y+(50-self.imageSize[1])))
+        self.x = x
+        self.y = y
+
+    def moveLeftBy(self,rate):
+        self.x-=rate
+        self.gameDisplay.blit(self.obstacle,(self.x,self.y+(50-self.imageSize[1])))
+        return self.x
